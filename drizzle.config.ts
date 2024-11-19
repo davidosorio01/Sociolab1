34b2c5e99817ac765e1a72ca61d1a697751c1480
David Osorio <davidoso7901@gmail.com>
2024-11-19 12:34:18 -0500

    mandar

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
 
 export default defineConfig({
 	out: './drizzle',
