ae493ac313bf8cfd265ba447332cf9de0d46acf9
David Osorio <davidoso7901@gmail.com>
2024-11-19 12:11:46 -0500

    enviar

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
 
 export default defineConfig({
 	out: './drizzle',
 	schema: './src/lib/server/database/data.ts',
 	dialect: 'turso',
 	dbCredentials: {
-		url: process.env.DATABASE_URL,
-		authToken: process.env.DATABASE_AUTH_TOKEN
+		url: process.env.DATABASE_URL!,
+		authToken: process.env.DATABASE_AUTH_TOKEN!
 	},
-	verbose: true,
-	strict: true,
 });
